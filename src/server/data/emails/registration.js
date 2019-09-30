1d5bccb6ba288208d56c77b8e1fd76c4df6004fe
anna-kuzii <anna_kuzij@ukr.net>
2019-09-30 16:11:21 +0300

    add email

--- src/server/data/emails/registration.js
+++ src/server/data/emails/registration.js
@@ -1,39 +1,39 @@
-export const registrationMsg = {
+export const registrationMail = {
   from: 'girls.sport.news@gmail.com',
   to: 'akuzii@softserveinc.com',
   subject: 'Success registration',
-  html: '<table style="border-collapse: collapse; background: #f9f9fb; position: absolute; width: 100%; right: 50%; transform: translateX(50%); border-bottom: 50px solid #f9f9fb;">\n' +
-    '    <thead>\n' +
-    '    <tr>\n' +
-    '        <td colspan="3" style="font-family: Open Sans; font-weight: 600; font-size: 24px; line-height: 33px; text-align: center; padding: 50px 0;">\n' +
-    '            Hello! You have successfully registered to Sport News <br /> March 21, 2019\n' +
-    '        </td>\n' +
-    '    </tr>\n' +
-    '    </thead>\n' +
-    '    <tbody style="background: #fff; text-align: center;">\n' +
-    '    <tr>\n' +
-    '        <td style="background: #f9f9fb; width: calc(100% / 3)"></td>\n' +
-    '        <td style="padding: 40px 0 15px; max-width: 400px">\n' +
-    '            <svg width="21" height="16" viewBox="0 0 21 16" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
-    '                <path d="M1 9L8 14.5L20 1" stroke="#D72130" stroke-width="2" stroke-linejoin="round"/>\n' +
-    '            </svg>\n' +
-    '        </td>\n' +
-    '        <td style="background: #f9f9fb; width: calc(100% / 3)"></td>\n' +
-    '    </tr>\n' +
-    '    <tr>\n' +
-    '        <td style="background: #f9f9fb; width: calc(100% / 3)"></td>\n' +
-    '        <td style="font-family: Open Sans; font-weight: 600; font-size: 22px; line-height: 28px; color: #D72130; padding: 0 0 15px; max-width: 400px">\n' +
-    '            Sport News\n' +
-    '        </td>\n' +
-    '        <td style="background: #f9f9fb; width: calc(100% / 3)"></td>\n' +
-    '    </tr>\n' +
-    '    <tr>\n' +
-    '        <td style="background: #f9f9fb; width: calc(100% / 3)"></td>\n' +
-    '        <td style="font-family: Open Sans; font-weight: 600; font-size: 12px; line-height: 16px; color: #596377; padding: 0 0 40px; max-width: 400px">\n' +
-    '            Now you will receive the latest news in the world of sport\n' +
-    '        </td>\n' +
-    '        <td style="background: #f9f9fb; width: calc(100% / 3)"></td>\n' +
-    '    </tr>\n' +
-    '    </tbody>\n' +
-    '</table>'
-};
+  html: `<table style="border-collapse: collapse; background: #f9f9fb; position: absolute; width: 100%; right: 50%; transform: translateX(50%); border-bottom: 50px solid #f9f9fb;">
+        <thead>
+        <tr>
+            <td colspan="3" style="font-family: Open Sans; font-weight: 600; font-size: 24px; line-height: 33px; text-align: center; padding: 50px 0;">
+                Hello! You have successfully registered to Sport News <br /> March 21, 2019\n' +
+            </td>
+        </tr>
+        </thead>
+        <tbody style="background: #fff; text-align: center;">
+        <tr>
+            <td style="background: #f9f9fb; width: calc(100% / 3)"></td>
+            <td style="padding: 40px 0 15px; max-width: 400px">
+                <svg width="21" height="16" viewBox="0 0 21 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 9L8 14.5L20 1" stroke="#D72130" stroke-width="2" stroke-linejoin="round"/>
+                </svg>
+            </td>
+            <td style="background: #f9f9fb; width: calc(100% / 3)"></td>
+        </tr>
+        <tr>
+            <td style="background: #f9f9fb; width: calc(100% / 3)"></td>
+            <td style="font-family: Open Sans; font-weight: 600; font-size: 22px; line-height: 28px; color: #D72130; padding: 0 0 15px; max-width: 400px">
+                Sport News
+            </td>
+            <td style="background: #f9f9fb; width: calc(100% / 3)"></td>
+        </tr>
+        <tr>
+            <td style="background: #f9f9fb; width: calc(100% / 3)"></td>
+            <td style="font-family: Open Sans; font-weight: 600; font-size: 12px; line-height: 16px; color: #596377; padding: 0 0 40px; max-width: 400px">
+                Now you will receive the latest news in the world of sport
+            </td>
+            <td style="background: #f9f9fb; width: calc(100% / 3)"></td>
+        </tr>
+        </tbody>
+    </table>`
+}
--- src/server/helpers/sendEmail/index.js
+++ src/server/helpers/sendEmail/index.js
@@ -1,2 +1,26 @@
+import registrationMail from '../../data/emails/registration';
+
 const nodemailer = require('nodemailer');
 
+async function main() {
+  nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "girls.sport.news",
+      pass: "SportNews@)19"
+    }
+  });
+
+  let info = await transporter.sendMail({
+    from: 'girls.sport.news',
+    to: 'akuzij@gmail.com',
+    subject: 'Success registration',
+    html: registrationMail
+  });
+
+  console.log('Message sent: %s', info.messageId);
+}
+
+main().catch(console.error);
